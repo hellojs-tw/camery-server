1eb0c797ab08477a32caff9083f2e805c7efbe08
dd-han <han@dd-han.tw>
2018-06-19 18:56:38 +0800

    adjust user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,28 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var User = sequelize.define('User', {
-    username: DataTypes.STRING
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+    },
+    avatar: {
+      type: DataTypes.STRING,
+    },
+    displayName: {
+      type: DataTypes.STRING,
+    },
   });
 
   return User;
